4e3e455f943c76580688adebff4f778201cf5dea
Luiz Felipe Calvo <luizfocalvo@estudante.ufla.br>
2021-03-29 21:17:21 -0300

    Removido atributo id do schema

--- tarefas-model.js
+++ tarefas-model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let TarefaSchema = new Schema({
-    id: {type: Number, required: true},
     descricao: {type: String,required: true},
     prazo: {type: Date,required: false},
     completa: {type: Boolean, required: false}
